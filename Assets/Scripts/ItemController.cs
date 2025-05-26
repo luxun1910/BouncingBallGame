b0041eb8b38dc354e95635bc55e89f674114a30c
luxun1910 <luxun.unanimity1910@gmail.com>
2025-05-26 14:08:23 +0800

    Use private constants, make some private methods, and refactor variant names

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -7,6 +7,59 @@ using System.Collections;
 /// </summary>
 public class ItemController : MonoBehaviour
 {
+    /// <summary>
+    /// アイテム生成のクールダウン時間
+    /// </summary>
+    private const float ITEM_SPAWN_COOLDOWN = 1.0f;
+
+    /// <summary>
+    /// 画面内に存在できるアイテムの最大数
+    /// </summary>
+    private const int MAX_ITEMS = 2;
+
+    /// <summary>
+    /// アイテム生成のX座標の最小値
+    /// </summary>
+    private const float X_RANGE_MIN = -2f;
+
+    /// <summary>
+    /// アイテム生成のX座標の最大値
+    /// </summary>
+    private const float X_RANGE_MAX = 2f;
+
+    /// <summary>
+    /// アイテム生成のY座標の最小値
+    /// </summary>
+    private const float Y_RANGE_MIN = -4f;
+
+    /// <summary>
+    /// アイテム生成のY座標の最大値
+    /// </summary>
+    private const float Y_RANGE_MAX = 4f;
+
+    /// <summary>
+    /// ミサイル生成のY座標
+    /// </summary>
+    private const float MISSILE_SPAWN_Y = 5f;
+
+    /// <summary>
+    /// アイテムのフェードアニメーションのアルファ値
+    /// </summary>
+    private const byte FADE_AMOUNT = 3;
+
+    /// <summary>
+    /// アイテムのフェードアニメーションの待機時間
+    /// </summary>
+    private const float FADE_WAIT = 0.1f;
+
+    /// <summary>
+    /// アイテムのフェードアニメーションのステップ数
+    /// </summary>
+    private const int FADE_STEPS = 85;
+
+    /// <summary>
+    /// アイテム生成の時間
+    /// </summary>
     private float dt;
 
     /// <summary>
@@ -49,49 +102,75 @@ public class ItemController : MonoBehaviour
 
         dt += Time.deltaTime;
 
-        // 一度アイテムを生成してから1秒間はアイテムを生成しない
-        if(dt < 1)
+        // クールダウン時間が経過していなければ処理しない
+        if (dt < ITEM_SPAWN_COOLDOWN)
         {
             return;
         }
 
-        // スローダウンアイテム
-        var randomX = UnityEngine.Random.Range(-2f, 2f);
-        var randomY = UnityEngine.Random.Range(-4f, 4f);
+        // アイテム生成を試行
+        TrySpawnItem();
+    }
 
-        var isSpawn = !System.Convert.ToBoolean(UnityEngine.Random.Range(0, (int)itemInstantiatePercentage));
-        if (isSpawn)
+    /// <summary>
+    /// アイテム生成を試行するメソッド
+    /// </summary>
+    private void TrySpawnItem()
+    {
+        // スローダウンアイテムの生成試行
+        if (TrySpawnRandomItem(slowDownItemPrefab, X_RANGE_MIN, X_RANGE_MAX, Y_RANGE_MIN, Y_RANGE_MAX))
+        {
+            return;
+        }
+
+        // スピードアップアイテムの生成試行
+        if (TrySpawnRandomItem(speedUpItemPrefab, X_RANGE_MIN, X_RANGE_MAX, Y_RANGE_MIN, Y_RANGE_MAX))
+        {
+            return;
+        }
+
+        // ミサイルの生成試行
+        TrySpawnMissile();
+    }
+
+    /// <summary>
+    /// ランダムなアイテム生成を試行するメソッド
+    /// </summary>
+    /// <param name="prefab">生成するアイテムのプレハブ</param>
+    /// <param name="xMin">生成位置のX座標の最小値</param>
+    /// <param name="xMax">生成位置のX座標の最大値</param>
+    /// <param name="yMin">生成位置のY座標の最小値</param>
+    private bool TrySpawnRandomItem(GameObject prefab, float xMin, float xMax, float yMin, float yMax)
+    {
+        float randomX = UnityEngine.Random.Range(xMin, xMax);
+        float randomY = UnityEngine.Random.Range(yMin, yMax);
+
+        bool shouldSpawn = !System.Convert.ToBoolean(UnityEngine.Random.Range(0, (int)itemInstantiatePercentage));
+        if (shouldSpawn)
         {
             dt = 0.0f;
-            var obj = Instantiate(slowDownItemPrefab, new Vector3(randomX, randomY, 0), Quaternion.identity);
+            GameObject obj = Instantiate(prefab, new Vector3(randomX, randomY, 0), Quaternion.identity);
             StartCoroutine(DestroyMethod(obj));
-            return;
+            return true;
         }
+        return false;
+    }
 
-        // スピードアップアイテム
-        var randomXForSpeed = UnityEngine.Random.Range(-2f, 2f);
-        var randomYForSpeed = UnityEngine.Random.Range(-4f, 4f);
+    /// <summary>
+    /// ミサイル生成を試行するメソッド
+    /// </summary>
+    private bool TrySpawnMissile()
+    {
+        float randomX = UnityEngine.Random.Range(X_RANGE_MIN, X_RANGE_MAX);
 
-        var isSpawnSpeed = !System.Convert.ToBoolean(UnityEngine.Random.Range(0, (int)itemInstantiatePercentage));
-        if (isSpawnSpeed)
+        bool shouldSpawn = !System.Convert.ToBoolean(UnityEngine.Random.Range(0, (int)itemInstantiatePercentage));
+        if (shouldSpawn)
         {
             dt = 0.0f;
-            var obj = Instantiate(speedUpItemPrefab, new Vector3(randomXForSpeed, randomYForSpeed, 0), Quaternion.identity);
-            StartCoroutine(DestroyMethod(obj));
-            return;
+            Instantiate(missilePrefab, new Vector3(randomX, MISSILE_SPAWN_Y, 0), Quaternion.LookRotation(Vector3.down));
+            return true;
         }
-        
-        // ミサイル（妨害アイテム）
-        var randomXForMissile = UnityEngine.Random.Range(-2f, 2f);
-
-        var isSpawnMissile = !System.Convert.ToBoolean(UnityEngine.Random.Range(0, (int)itemInstantiatePercentage));
-        if (isSpawnMissile)
-        {
-            dt = 0.0f;
-            var obj = Instantiate(missilePrefab, new Vector3(randomXForMissile, 5, 0), Quaternion.LookRotation(Vector3.down));
-            return;
-        }
-
+        return false;
     }
 
     /// <summary>
@@ -100,10 +179,10 @@ public class ItemController : MonoBehaviour
     /// <param name="obj">消滅するアイテムのオブジェクト</param>
     private IEnumerator DestroyMethod(GameObject obj)
     {
-        for (var i = 0; i < 85; i++)
+        for (var i = 0; i < FADE_STEPS; i++)
         {
-            obj.GetComponent<Renderer>().material.color -= new Color32(0, 0, 0, 3);
-            yield return new WaitForSeconds(0.1f);
+            obj.GetComponent<Renderer>().material.color -= new Color32(0, 0, 0, FADE_AMOUNT);
+            yield return new WaitForSeconds(FADE_WAIT);
         }
 
         Destroy(obj);
--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -26,6 +26,14 @@ public class LineController : MonoBehaviour
     /// </summary>
     public float lineWidth = 0.1f;
 
+    /// <summary>
+    /// ラインの破棄遅延時間
+    /// </summary>
+    private const float LINE_DESTROY_DELAY = 2.0f;
+
+    /// <summary>
+    /// 予測タッチ位置
+    /// </summary>
     private Vector3 predictionTouchPos;
 
     /// <summary>
@@ -40,75 +48,155 @@ public class LineController : MonoBehaviour
 
     void Update()
     {
-        drawLine();
+        HandleLineDrawing();
     }
 
-    void drawLine()
+    /// <summary>
+    /// ライン描画を処理する
+    /// </summary>
+    private void HandleLineDrawing()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            touchPos.z = 0;
-
-            predictionTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            predictionTouchPos.z = 0;
+            HandleTouchStart();
         }
 
         if (Input.GetMouseButton(0))
         {
-            Vector3 startPos = predictionTouchPos;
-            Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            endPos.z = 0;
-
-            if ((endPos - startPos).magnitude > lineLength)
-            {
-                GameObject obj = Instantiate(predictionLinePrefab, transform.position, transform.rotation) as GameObject;
-                obj.transform.position = (startPos + endPos) / 2;
-                obj.transform.right = (endPos - startPos).normalized;
-
-                obj.transform.localScale = new Vector3((endPos - startPos).magnitude, lineWidth, lineWidth);
-
-                obj.transform.parent = this.transform;
-
-                predictionTouchPos = endPos;
-
-            }
+            HandleTouchHold();
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            var predictionLines = GameObject.FindGameObjectsWithTag("PredictionLine");
-            foreach(var line in predictionLines)
-            {
-                Destroy(line);
-            }
-
-            Vector3 startPos = touchPos;
-            Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            endPos.z = 0;
-
-            if ((endPos - startPos).magnitude > lineLength)
-            {
-                GameObject obj = Instantiate(linePrefab, transform.position, transform.rotation);
-                obj.transform.position = (startPos + endPos) / 2;
-                obj.transform.right = (endPos - startPos).normalized;
-
-                obj.transform.localScale = new Vector3((endPos - startPos).magnitude, lineWidth, lineWidth);
-
-                obj.transform.parent = this.transform;
-
-                touchPos = endPos;
-
-                StartCoroutine(DestroyMethod(obj));
-            }
+            HandleTouchEnd();
         }
-
     }
 
-    private IEnumerator DestroyMethod(Object obj)
+    /// <summary>
+    /// タッチ開始時の処理
+    /// </summary>
+    private void HandleTouchStart()
     {
-        yield return new WaitForSeconds(2.0f);
+        touchPos = GetWorldTouchPosition();
+        predictionTouchPos = GetWorldTouchPosition();
+    }
 
+    /// <summary>
+    /// タッチ中（ドラッグ中）の処理
+    /// </summary>
+    private void HandleTouchHold()
+    {
+        Vector3 startPos = predictionTouchPos;
+        Vector3 endPos = GetWorldTouchPosition();
+
+        if (IsLineLongEnough(startPos, endPos))
+        {
+            CreatePredictionLine(startPos, endPos);
+            predictionTouchPos = endPos;
+        }
+    }
+
+    /// <summary>
+    /// タッチ終了時の処理
+    /// </summary>
+    private void HandleTouchEnd()
+    {
+        ClearPredictionLines();
+
+        Vector3 startPos = touchPos;
+        Vector3 endPos = GetWorldTouchPosition();
+
+        if (IsLineLongEnough(startPos, endPos))
+        {
+            GameObject line = CreateLine(startPos, endPos);
+            touchPos = endPos;
+            StartCoroutine(DestroyWithDelay(line));
+        }
+    }
+
+    /// <summary>
+    /// 予測線を作成
+    /// </summary>
+    /// <param name="start">開始位置</param>
+    /// <param name="end">終了位置</param>
+    /// <returns>作成した予測線のオブジェクト</returns>
+    private GameObject CreatePredictionLine(Vector3 start, Vector3 end)
+    {
+        GameObject obj = Instantiate(predictionLinePrefab, transform.position, transform.rotation);
+        ConfigureLine(obj, start, end);
+        obj.transform.parent = this.transform;
+        return obj;
+    }
+
+    /// <summary>
+    /// 実際の線を作成
+    /// </summary>
+    /// <param name="start">開始位置</param>
+    /// <param name="end">終了位置</param>
+    /// <returns>作成した実際の線のオブジェクト</returns>
+    private GameObject CreateLine(Vector3 start, Vector3 end)
+    {
+        GameObject obj = Instantiate(linePrefab, transform.position, transform.rotation);
+        ConfigureLine(obj, start, end);
+        obj.transform.parent = this.transform;
+        return obj;
+    }
+
+    /// <summary>
+    /// 線のプロパティ設定
+    /// </summary>
+    /// <param name="lineObject">線のオブジェクト</param>
+    /// <param name="start">開始位置</param>
+    /// <param name="end">終了位置</param>
+    private void ConfigureLine(GameObject lineObject, Vector3 start, Vector3 end)
+    {
+        lineObject.transform.position = (start + end) / 2;
+        lineObject.transform.right = (end - start).normalized;
+        lineObject.transform.localScale = new Vector3((end - start).magnitude, lineWidth, lineWidth);
+    }
+
+    /// <summary>
+    /// タッチ位置をワールド座標に変換
+    /// </summary>
+    /// <returns>ワールド座標に変換されたタッチ位置</returns>
+    private Vector3 GetWorldTouchPosition()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        return pos;
+    }
+
+    /// <summary>
+    /// 予測線をクリア
+    /// </summary>
+    private void ClearPredictionLines()
+    {
+        var predictionLines = GameObject.FindGameObjectsWithTag(GlobalVariants.Tags.PredictionLine);
+        foreach (var line in predictionLines)
+        {
+            Destroy(line);
+        }
+    }
+
+    /// <summary>
+    /// 線の長さが十分かチェック
+    /// </summary>
+    /// <param name="start">開始位置</param>
+    /// <param name="end">終了位置</param>
+    /// <returns>線の長さが十分かどうか</returns>
+    private bool IsLineLongEnough(Vector3 start, Vector3 end)
+    {
+        return (end - start).magnitude > lineLength;
+    }
+
+    /// <summary>
+    /// 遅延破棄用コルーチン
+    /// </summary>
+    /// <param name="obj">破棄するオブジェクト</param>
+    /// <returns>破棄用コルーチン</returns>
+    private IEnumerator DestroyWithDelay(Object obj)
+    {
+        yield return new WaitForSeconds(LINE_DESTROY_DELAY);
         Destroy(obj);
     }
 }
