85fd3147f8fcfd4d6c7104c1fc7130a8c8a75066
luxun1910 <luxun.unanimity1910@gmail.com>
2025-05-26 14:03:28 +0800

    Make magic number private variant

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 /// </summary>
 public class ExplosionController : MonoBehaviour
 {
+    /// <summary>
+    /// 爆発オブジェクトの寿命
+    /// </summary>
+    private const float EXPLOSION_LIFETIME = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 2.0f);
+        Destroy(this.gameObject, EXPLOSION_LIFETIME);
     }
 
     // Update is called once per frame
